e7be710dbddc68625751a2eccf1013632b1a6be5
Notebook <omarokstar2001@gmail.com>
2025-06-21 11:36:47 -0700

    last

--- medical_stores/views.py
+++ medical_stores/views.py
@@ -1,8 +1,16 @@
+# medical_stores/views.py
 from rest_framework import viewsets
 from .models import MedicalStore
 from .serializers import MedicalStoreSerializer
+from .filters import MedicalStoreFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
-# MEDICAL DEVICE VIEWSET
 class MedicalStoreViewSet(viewsets.ModelViewSet):
     queryset = MedicalStore.objects.all()
     serializer_class = MedicalStoreSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = MedicalStoreFilter
+
+    search_fields = ['store_name', 'store_type']
