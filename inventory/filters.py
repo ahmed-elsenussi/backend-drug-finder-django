16b48c00e45f6f9c07ccbee14045d12d31312fe9
ahmed-kamal91 <nagmstar91@gmail.com>
2025-06-28 14:06:01 +0300

    make some handlings

--- inventory/filters.py
+++ inventory/filters.py
@@ -1,3 +1,4 @@
+# inventory/filters.py
 import django_filters
 from .models import Medicine
 
@@ -5,7 +6,8 @@ class MedicineFilter(django_filters.FilterSet):
     brand_startswith = django_filters.CharFilter(
         field_name="brand_name", lookup_expr="istartswith"
     )
+    store_id = django_filters.NumberFilter(field_name="store__id")
 
     class Meta:
         model = Medicine
-        fields = ['store']  
+        fields = ['store_id', 'brand_startswith']
