bd18d6075abc777cc567a0e9429f8f1744add539
Notebook <omarokstar2001@gmail.com>
2025-07-01 20:10:01 -0700

    remove-cart-order-model

--- orders/admin.py
+++ orders/admin.py
@@ -1,19 +1,4 @@
 from django.contrib import admin
-from .models import ShippingConfig, TaxConfig
 
 
-@admin.register(ShippingConfig)
-class ShippingConfigAdmin(admin.ModelAdmin):
-    list_display = ('name', 'max_distance_km', 'cost', 'is_active')
-    list_editable = ('cost', 'is_active')
-    ordering = ('max_distance_km',)
-
-@admin.register(TaxConfig)
-class TaxConfigAdmin(admin.ModelAdmin):
-    list_display = ('region_name', 'tax_rate_percentage', 'is_active')
-    list_editable = ('is_active',)
-    
-    def tax_rate_percentage(self, obj):
-        return f"{float(obj.tax_rate)*100:.2f}%"
-    tax_rate_percentage.short_description = 'Tax Rate'
 # Register your models here.
--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, CartViewSet
-
+from .views import OrderViewSet
 # Separate routers
 order_router = DefaultRouter()
 order_router.register(r'', OrderViewSet, basename='order')  # will be  at /orders/
-
-cart_router = DefaultRouter()
-cart_router.register(r'', CartViewSet, basename='cart')  # will be  at /cart/
